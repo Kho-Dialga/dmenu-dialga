57201538030746b514d5b03303d58bc76f28300e
Kho-Dialga <ivandashenyou@gmail.com>
2020-09-29 12:39:34 -0600

    Made the emojis a bit bigger

--- config.h
+++ config.h
@@ -7,7 +7,7 @@ static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"monospace:size=12",
-	"Noto Color Emoji:pixelsize=10:antialias=true:autohint=true"
+	"Noto Color Emoji:pixelsize=15:antialias=true:autohint=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
